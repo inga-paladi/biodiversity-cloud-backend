a972d27566b15d506e140bb5ff48648f88ca1c09
moonbye <inga-paladi>
2025-05-12 18:34:02 +0300

    add drop script for dropping all db tables

---
+++ BiodiversityCloudApp/drop-script.sql
@@ -0,0 +1,6 @@
+DROP TABLE public."Photos";
+DROP TABLE public."ObservationRecords";
+DROP TABLE public."Observations";
+DROP TABLE public."Users";
+DROP TABLE public."Animals";
+DROP TABLE public."__EFMigrationsHistory";
